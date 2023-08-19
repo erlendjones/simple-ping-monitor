234e4575e85e6be358cab797212f686b0c3f7fce
Erlend Jones <erlend.jones@gmail.com>
2023-08-20 00:32:28 +0200

    karpe-190823

--- src/index.ts
+++ src/index.ts
@@ -28,23 +28,43 @@ const ping = (ip: string, name: string): Promise<any> => {
     })
     .then((res: any) => {
       if (res?.reply?.typestr === "ECHO_REPLY")
-        resolve(chalk.green(ip + " [" + name + "]: UP"));
-      else resolve(chalk.red(ip + " [" + name + "]: DOWN"));
+        resolve(chalk.greenBright(ip + " [" + name + "]: UP"));
+      else resolve(chalk.redBright(ip + " [" + name + "]: DOWN"));
     }))
 };
 
-// Artnet
+let result: any[] = [];
+let resultTimestamp: number = 0;
+let resultInflight = false;
+const run = () => {
+  resultInflight = true
+  Promise.all([
+    // Artnet
+    ping("2.0.0.21", "ARTNET: Video"),
+    ping("2.0.0.50", "ARTNET: AJA FS HDR"),
+    ping("2.0.0.51", "ARTNET: XLIT Mac"),
+    ping("2.0.0.101", "ARTNET: GrandMA 2"),
+    // Dante
+    ping("192.168.1.237", "DANTE: Imag IO1"),
+    ping("192.168.1.238", "DANTE: Imag IO2"),
+    ping("192.168.1.239", "DANTE: XLIT Mac"),
+    ping("192.168.1.196", "DANTE: Tracks"),
+  ]).then((results: any[]) => {
+    resultInflight = false;
+    result = results;
+    resultTimestamp = Date.now()
+    print()
+  })
+}
 
-Promise.all([
-  ping("2.0.0.21", "ARTNET: Video"),
-  ping("2.0.0.50", "ARTNET: AJA FS HDR"),
-  ping("2.0.0.51", "ARTNET: XLIT Mac"),
-  ping("2.0.0.101", "ARTNET: GrandMA 2"),
-  // Dante
-  ping("192.168.1.237", "DANTE: Imag IO1"),
-  ping("192.168.1.238", "DANTE: Imag IO2"),
-  ping("192.168.1.239", "DANTE: XLIT Mac"),
-  ping("192.168.1.196", "DANTE: Tracksbox"),
-]).then((results: any[]) => {
-  results.forEach(res => console.log(res))
-})
+const print = () => {
+  console.clear()
+  if (resultInflight) console.log(chalk.blueBright('Running...'))
+  else console.log(chalk.blueBright(Math.round((Date.now() - resultTimestamp) / 1000) + ' seconds ago...'))
+  result.forEach(res => console.log(res))
+}
+
+// init
+run()
+setInterval(run, 10000)
+setInterval(print, 1000)
