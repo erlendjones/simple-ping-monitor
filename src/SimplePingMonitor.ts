8870ec9fd703ddcadcfd3bfd48f0d7cbe57ce196
Erlend Jones <erlend.jones@gmail.com>
2023-08-20 17:35:30 +0200

    v0.2

---
+++ src/SimplePingMonitor.ts
@@ -0,0 +1,231 @@
+import chalk from "chalk";
+import logSymbols from "log-symbols";
+import pingus from "pingus";
+import fetch from "node-fetch";
+const figlet = require("figlet");
+
+type Machine = [ip: string, name: string, category?: string];
+type Result = { machine: Machine; result: any };
+export class SimplePingMonitor {
+  runInterval: NodeJS.Timer;
+  printInterval: NodeJS.Timer;
+  result: Result[];
+  resultTimestamp: any;
+  resultInflight: boolean;
+  machines: Machine[];
+  lenIP: number;
+  lenName: number;
+
+  constructor(
+    machines: Machine[],
+    options?: {
+      interval: number; // ms
+    }
+  ) {
+    this.machines = machines;
+
+    // get max lengths for printing
+    this.lenIP = Math.max(...this.machines.map((m) => m[0].length));
+    this.lenName = Math.max(...this.machines.map((m) => m[1].length));
+
+    // reset
+    this.result = [];
+    this.resultTimestamp = 0;
+    this.resultInflight = false;
+
+    // init
+    this.runInterval = setInterval(this.run, options?.interval);
+    this.printInterval = setInterval(this.print, 1000);
+
+    // pre-run
+    this.run();
+    this.print();
+  }
+
+  run = async () => {
+    this.resultInflight = true;
+
+    // run all machines
+    let complete = 0;
+    for (let i = 0; i < this.machines.length; i++) {
+      // init result
+      if (this.result[i] === undefined)
+        this.result[i] = {
+          machine: this.machines[i],
+          result: this.line(
+            null,
+            this.machines[i][0],
+            this.machines[i][1],
+            "Running..."
+          ),
+        };
+
+      // run
+      this.process(...this.machines[i]).then((result: Result) => {
+        // save result
+        this.result[i] = result;
+
+        // count complete
+        complete++;
+
+        // when all machines are complete, reset
+        if (complete === this.machines.length) {
+          this.resultInflight = false;
+          this.resultTimestamp = Date.now();
+        }
+
+        // do print
+        this.print();
+      });
+    }
+  };
+
+  pad = (str: string, len: number): string => {
+    if (str.length < len) {
+      str = str + " ".repeat(len - str.length);
+    }
+    return str;
+  };
+
+  print = () => {
+    // reset console
+    console.clear();
+    console.log(figlet.textSync("PingMonitor"));
+    console.log();
+
+    // sort by category
+    let lastCategory = "";
+
+    // print results
+    this.result.forEach((res: Result) => {
+      // print category
+      if (res.machine[2] !== lastCategory) {
+        console.log();
+        console.log("   " + chalk.gray(res.machine[2]));
+        lastCategory = res.machine[2] || "";
+      }
+
+      // print result
+      console.log(res.result);
+    });
+
+    // print footer
+    console.log();
+
+    // print time since last run
+    if (this.resultInflight || this.resultTimestamp === 0)
+      console.log("   " + chalk.blueBright("Running..."));
+    else
+      console.log(
+        "   " +
+          chalk.blueBright(
+            Math.round((Date.now() - this.resultTimestamp) / 1000) +
+              " seconds ago..."
+          )
+      );
+  };
+
+  ping = (ip: string, name: string, category?: string): Promise<any> => {
+    return new Promise((resolve, reject) =>
+      pingus
+        .icmp({
+          host: ip,
+        })
+        .then((res: any) => {
+          if (res?.reply?.typestr === "ECHO_REPLY")
+            resolve({
+              machine: [ip, name, category],
+              result: this.line(true, ip, name),
+            });
+          else
+            resolve({
+              machine: [ip, name, category],
+              result: this.line(false, ip, name),
+            });
+        })
+    );
+  };
+
+  http = (ip: string, name: string, category?: string): Promise<any> => {
+    return new Promise((resolve, reject) =>
+      fetch(ip, {
+        type: "GET",
+        timeout: 3000,
+      })
+        .then((res: any) => {
+          if (res?.status === 200)
+            resolve({
+              machine: [ip, name, category],
+              result: this.line(
+                true,
+                ip,
+                name,
+                res.status + " " + res.statusText
+              ),
+            });
+          else
+            resolve({
+              machine: [ip, name, category],
+              result: chalk.red(
+                this.line(false, ip, name, res.status + " " + res.statusText)
+              ),
+            });
+        })
+        .catch((err: Error) => {
+          resolve({
+            machine: [ip, name, category],
+            result: this.line(false, ip, name, err.name),
+          });
+        })
+    );
+  };
+
+  line = (
+    succes: boolean | null,
+    ip: string,
+    name: string,
+    extra?: string
+  ): any => {
+    if (succes === null)
+      return (
+        " " +
+        logSymbols.warning +
+        " " +
+        chalk.white(this.pad(name, this.lenName)) +
+        " " +
+        chalk.gray(
+          this.pad("(" + ip + ")", this.lenIP + 2) + " " + (extra || "")
+        )
+      );
+    if (succes)
+      return (
+        " " +
+        logSymbols.success +
+        " " +
+        chalk.white(this.pad(name, this.lenName)) +
+        " " +
+        chalk.gray(
+          this.pad("(" + ip + ")", this.lenIP + 2) + " " + (extra || "")
+        )
+      );
+    else
+      return chalk.red(
+        " " +
+          logSymbols.error +
+          " " +
+          this.pad(name, this.lenName) +
+          " " +
+          this.pad("(" + ip + ")", this.lenIP + 2) +
+          " " +
+          (extra || "")
+      );
+  };
+
+  process = (ip: string, name: string, category?: string): Promise<any> => {
+    if (ip.includes("http")) {
+      return this.http(ip, name, category);
+    } else {
+      return this.ping(ip, name, category);
+    }
+  };
+}
--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,24 @@
-import chalk from "chalk";
-import { Command } from "commander";
+import { SimplePingMonitor } from "./SimplePingMonitor";
 
-import Debug from "./utils/debug";
-import { msToTc } from "./utils/msToTc";
-const figlet = require("figlet");
-const debug = Debug("CORE");
-
-const program = new Command();
-
-type SongTitle = string;
-
-const queue: SongTitle[] = [];
-const inFlight: SongTitle[] = [];
-
-console.log(figlet.textSync("NetMon"));
-
-const options = program.opts();
-
-import pingus from "pingus"; // ESM, Typescript
-
-// http Get
-
-const ping = (ip: string, name: string): Promise<any> => {
-  return new Promise((resolve, reject) => pingus
-    .icmp({
-      host: ip,
-    })
-    .then((res: any) => {
-      if (res?.reply?.typestr === "ECHO_REPLY")
-        resolve(chalk.greenBright(ip + " [" + name + "]: UP"));
-      else resolve(chalk.redBright(ip + " [" + name + "]: DOWN"));
-    }))
-};
-
-let result: any[] = [];
-let resultTimestamp: number = 0;
-let resultInflight = false;
-const run = () => {
-  resultInflight = true
-  Promise.all([
+new SimplePingMonitor(
+  [
     // Artnet
-    ping("2.0.0.21", "ARTNET: Video"),
-    ping("2.0.0.50", "ARTNET: AJA FS HDR"),
-    ping("2.0.0.51", "ARTNET: XLIT Mac"),
-    ping("2.0.0.101", "ARTNET: GrandMA 2"),
+    ["2.0.0.21", "FOH: Resolume-Mac", "Artnet"],
+    ["2.0.0.101", "FOH: GrandMA2", "Artnet"],
+    ["2.0.0.50", "OB: AJA FS HDR", "Artnet"],
+    ["2.0.0.51", "OB: Mac Mini", "Artnet"],
+
     // Dante
-    ping("192.168.1.237", "DANTE: Imag IO1"),
-    ping("192.168.1.238", "DANTE: Imag IO2"),
-    ping("192.168.1.239", "DANTE: XLIT Mac"),
-    ping("192.168.1.196", "DANTE: Tracks"),
-  ]).then((results: any[]) => {
-    resultInflight = false;
-    result = results;
-    resultTimestamp = Date.now()
-    print()
-  })
-}
+    ["192.168.1.237", "OB: Imag IO1", "Dante"],
+    ["192.168.1.238", "OB: Imag IO2", "Dante"],
+    ["192.168.1.239", "OB: Mac Mini", "Dante"],
+    ["192.168.1.196", "STAGE: Tracks", "Dante"],
 
-const print = () => {
-  console.clear()
-  if (resultInflight) console.log(chalk.blueBright('Running...'))
-  else console.log(chalk.blueBright(Math.round((Date.now() - resultTimestamp) / 1000) + ' seconds ago...'))
-  result.forEach(res => console.log(res))
-}
-
-// init
-run()
-setInterval(run, 10000)
-setInterval(print, 1000)
+    // Services
+    ["http://127.0.0.1:3345", "Voice-server", "Services"],
+    // when requesting a http-endpoint, it looks for a 200-response
+  ],
+  {
+    interval: 10000,
+  }
+);
